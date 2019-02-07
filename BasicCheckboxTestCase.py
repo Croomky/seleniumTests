54fee4357bf037beacadcfddcef0a3dd79faf794
unknown <konrad96pawlak@gmail.com>
2019-02-07 17:03:30 +0100

    basic checkbox

---
+++ BasicCheckboxTestCase.py
@@ -0,0 +1,29 @@
+import unittest
+
+from BaseSuite import BaseSuite
+from PageObjectBasicCheckbox import PageObjectBasicCheckbox
+from WebDriverFactory import WebDriverFactory
+
+class BasicCheckboxTestCase(unittest.TestCase):
+    def test_single_checkbox(self):
+        with BaseSuite(WebDriverFactory.E_browser.Firefox) as driver:
+            page_objects = PageObjectBasicCheckbox(driver)
+
+            if not page_objects.click_on_this_checkbox.is_selected():
+                page_objects.click_on_this_checkbox.click()
+            
+            style_attr = page_objects.success_message.get_attribute("style")
+            print('DEBUG: ' + page_objects.success_message.get_attribute("class"))
+            self.assertNotEqual(-1, style_attr.find('block'))
+            page_objects.click_on_this_checkbox.click()
+            self.assertNotEqual(-1, page_objects.success_message.get_attribute('style').find('none'))
+
+    def test_multiple_checkboxes(self):
+        with BaseSuite(WebDriverFactory.E_browser.Firefox) as driver:
+            page_objects = PageObjectBasicCheckbox(driver)
+
+            for i in page_objects.option_checkboxes:
+                self.assertEqual(page_objects.check_btn.get_attribute('value'), 'Check All')
+                i.click()
+            
+            self.assertEqual(page_objects.check_btn.get_attribute('value'), 'Uncheck All')
---
+++ PageObjectBasicCheckbox.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+class PageObjectBasicCheckbox:
+
+    def __init__(self, driver: webdriver):
+        self.driver = driver
+        self.driver.get('https://www.seleniumeasy.com/test/basic-checkbox-demo.html')
+
+        self.click_on_this_checkbox = self.driver.find_element_by_id('isAgeSelected')
+        self.success_message = self.driver.find_element_by_id('txtAge')
+
+        self.option_checkboxes = self.driver.find_elements_by_class_name('cb1-element')
+        self.check_btn = self.driver.find_element_by_id('check1')
